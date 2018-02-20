de7dc818990860eaeb9235cfad50ea1acead5a7c
JanessaMunt <janessamunt@gmail.com>
2018-02-20 02:25:12 -0800

    fills in real time and links

--- wiki.js
+++ wiki.js
@@ -1,17 +1,22 @@
 
-$.ajax({
-	url: "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=test&format=json&origin=*",
-	success: function(info){
-		$("#test").html(info.query.search[0].snippet);
-		console.log(info);
 
-		$("putStuff").html("<li>" + info.query.search[0].snippet + "</li>");
-		for(var i=0; i<10; i++){
-			$("#putStuff").append("<li>" + info.query.search[i].snippet + "</li>");
+$("#input").on( "input", function(){
+	console.log($("#input").val());
+	$.ajax({
+		url: "https://en.wikipedia.org/w/api.php?action=query&prop=info|extracts&inprop=url&list=search&srsearch=" + $("#input").val() + "&format=json&origin=*",
+		success: function(info){
+
+			$("#putStuff").text("");
+			console.log(info);
+
+			//make li with href
+			for(var i=0; i<10; i++){
+				$("#putStuff").append("<a href=\"https://en.wikipedia.org/?curid=" +  info.query.search[i].pageid + "\"> <li><span class=\"title\">" + info.query.search[i].title+ "</span><br>" + info.query.search[i].snippet + "</li></a>");
+			}
+
+
 		}
-		//make li with href
-
-	}
 
 
+	});
 });
